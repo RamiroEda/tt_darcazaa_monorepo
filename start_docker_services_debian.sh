271a7e22744cc0d4728ad9930dc49e7fd0505bae
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2022-05-11 00:42:09 -0500

    Update start_docker_services_debian.sh

--- start_docker_services_debian.sh
+++ start_docker_services_debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo HOST_IP=$(hostname -I | awk '{print $1}') >> .env
-cp /etc/wpa_supplicant/wpa_supplicant.conf ./mavlink_controller_server/wpa_supplicant.conf
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf ./mavlink_controller_server/wpa_supplicant.conf
 sudo docker-compose --env-file ./.env up
