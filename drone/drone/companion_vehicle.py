61dc7639c337f739d4738b6a2f06bf746501c457
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2022-05-18 18:46:33 -0500

    docker

--- drone/drone/companion_vehicle.py
+++ drone/drone/companion_vehicle.py
@@ -21,6 +21,7 @@ class CompanionVehicle:
         while True:
             if self.vehicle is None:
                 try:
+                    playsound(os.path.join(root, "start.mp3"))
                     print("Connecting to drone at localhost...")
                     self.vehicle = connect("127.0.0.1:14550", wait_ready=True)
                     print("Drone connected")
--- drone/run.sh
+++ drone/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make run
+docker run --device /dev/snd app
